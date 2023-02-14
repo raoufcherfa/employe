75a8ff84f321a2c30b9ade43ca0c05f2b81279ee
Abderaouf CHERFA <rcherfa@finaxys.com>
2023-02-15 00:28:03 +0100

    init project

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,16 @@
+
+:
+
+Dockerfile,6\6\6651ddff6eb82c840ced7c1dddee15c6e1913dd4
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+C
+employee-python.iml,d\f\dfef5fe9766fd642977295bd77a99da56f10fb75
+6
+app.py,1\3\13cce7fd076299c81b4986166f3d822791c9490e
+<
+unit_test.py,a\c\acb77de637b8f2b9b8535f462d35da0062be6f70
+@
+requirements.txt,1\9\19359a61ae2446b51b549167b014da2fcf265768
+;
+Jenkinsfile,0\c\0c2c5c506cef0ed2d3ad2b7e45e97980b66f5520
---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+employees = [
+    {
+        'id': 1,
+        'firstName': 'Karim',
+        'lastName': 'Benzema',
+        'emailId': 'Benzema@gmail.com'
+    },
+    {
+        'id': 2,
+        'firstName': 'Leo',
+        'lastName': 'Messi',
+        'emailId': 'Messi@gmail.com'
+    },
+    {
+        'id': 3,
+        'firstName': 'Ronaldo',
+        'lastName': 'Cristiano',
+        'emailId': 'Cristiano@gmail.com'
+    },
+]
+
+@app.route('/said/mikail', methods=['GET'])
+def get_said():
+    return "Happy Birthday Lenny :)"
+
+@app.route('/api/v1/employees', methods=['GET', 'POST'])
+def manage_employees():
+    if request.method == 'GET':
+        return jsonify(employees)
+
+    if request.method == 'POST':
+        employee = request.get_json()
+        employee['id'] = employees[-1]['id'] + 1
+        employees.append(employee)
+        return jsonify(employee), 201
+
+@app.route('/api/v1/employees/<int:employee_id>', methods=['GET', 'DELETE'])
+def manage_employee(employee_id):
+    employee = [employee for employee in employees if employee['id'] == employee_id]
+    if len(employee) == 0:
+        return jsonify({'error': 'employee not found'})
+
+    if request.method == 'GET':
+        return jsonify(employee[0])
+
+    if request.method == 'DELETE':
+        employees.remove(employee[0])
+        return jsonify({'result': 'employee deleted'})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8081)
